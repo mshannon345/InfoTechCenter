d455170c558a19dae6fa7187cf09b084118eb24f
Shannon <milesshannon151@gmail.com>
2024-02-09 14:05:18 -0500

    Import Time

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,3 +1,9 @@
 
+#Import Libraries Here
+import time
+
+timeToSleep = 4
+
 print("\n\nWelcome to InfoTech Center V1.0")
+time.sleep(timeToSleep)
 print("\nInfoTech Center Operation System Loading...")
